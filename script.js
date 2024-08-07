49d82be759ada21523aa937a06ca3eee251e2115
Somak Mahapatra <122389256+Jaishreeram49@users.noreply.github.com>
2024-08-08 02:27:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 $(document).ready(function(){
     $('#menu').click(function(){
         $(this).toggleClass('fa-times');
@@ -8,9 +7,31 @@ $(document).ready(function(){
 
     $(window).on('scroll load',function(){
         $('#menu').removeClass('fa-times');
-        $('header').removeClass('togle');
+        $('header').removeClass('toggle');
 
-
+        if($(window).scrollTop() > 0){
+            $('.top').show();
+        }else{
+            $('.top').hide();
+        }
     });
 
+    // smooth scrolling 
+    $('a[href*="#"]').on('click',function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop : $($(this).attr('href')).offset().top,
+        },
+        500, 
+        'linear'
+        );
+    });
+});
+
+// Typing animation
+var typed = new Typed(".typing", {
+    strings: ["Somak Mahapatra", "Web Developer", "MERN Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
 });
