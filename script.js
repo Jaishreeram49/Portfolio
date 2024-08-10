a5129b6d6866a62370c6b002fc3f2c8e44792be6
Somak Mahapatra <122389256+Jaishreeram49@users.noreply.github.com>
2024-08-11 01:06:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,52 @@
 $(document).ready(function(){
-  var typed = new Typed('#typed-text', {
-    strings: ['Somak Mahapatra', 'a Web Developer', 'a MERN Stack Developer'],
-    typeSpeed: 50,
-    backSpeed: 50,
-    loop: true
-});
-
-  $('#menu').click(function(){
-      $(this).toggleClass('fa-times');
-      $('header').toggleClass('toggle');
-      $('body').toggleClass('menu-open');
+    var typed = new Typed('#typed-text', {
+      strings: ['Somak Mahapatra', 'a Web Developer', 'a MERN Stack Developer'],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true
   });
-  $(window).on('scroll load',function(){
-      $('#menu').removeClass('fa-times');
-      $('header').removeClass('togle');
+  
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+        $('body').toggleClass('menu-open');
+    });
+    $(window).on('scroll load',function(){
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('togle');
+    });
+    $('a[href^="#"]').on('click', function(e) {
+      e.preventDefault();
+      var target = $(this.hash);
+      $('html, body').animate({
+          scrollTop: target.offset().top
+      }, 800);
   });
-  $('a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
-    var target = $(this.hash);
-    $('html, body').animate({
-        scrollTop: target.offset().top
-    }, 800);
-});
-$(window).scroll(function() {
-  $('section').each(function() {
-      if ($(window).scrollTop() + $(window).height() > $(this).offset().top + 100) {
-          $(this).addClass('fade-in');
-      }
+  $(window).scroll(function() {
+    $('section').each(function() {
+        if ($(window).scrollTop() + $(window).height() > $(this).offset().top + 100) {
+            $(this).addClass('fade-in');
+        }
+    });
   });
-});
-$('.skill-bar').each(function() {
-  $(this).find('.bar').animate({
-      width: $(this).attr('data-percent')
-  }, 2000);
-});
-
-});
-
+  $('.skill-bar').each(function() {
+    $(this).find('.bar').animate({
+        width: $(this).attr('data-percent')
+    }, 2000);
+  });
+  
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        // Prevent the form from submitting normally
+        event.preventDefault();
+        
+        // Show an alert (you can customize this alert message)
+        alert('Thank you for your message! We will get back to you soon.');
+        
+        // Refresh the page after 2 seconds
+        setTimeout(function() {
+            location.reload();
+        }, 2000);
+    });
+});  
